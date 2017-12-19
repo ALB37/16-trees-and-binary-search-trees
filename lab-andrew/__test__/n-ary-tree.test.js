13900787eb3a23b555e96622dee3d50b74fc93b7
Andrew Bloom <blooma116@gmail.com>
2017-12-18 18:20:08 -0800

    added tests and two functions for n-ary-tree

---
+++ lab-andrew/__test__/n-ary-tree.test.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const NAryTree = require('../lib/n-ary-tree');
+
+describe('tests for n-ary-tree.js', () => {
+  const root = new NAryTree(0);
+  const one = new NAryTree(1);
+  const two = new NAryTree(2);
+  const three = new NAryTree(3);
+  const four = new NAryTree(4);
+  const five = new NAryTree(5);
+  const six = new NAryTree(6);
+  const seven = new NAryTree(7);
+  const eight = new NAryTree(8);
+  const doubleEight = new NAryTree(8);
+  
+  root.appendChild(one);
+  root.appendChild(two);
+  root.appendChild(three);
+  two.appendChild(four);
+  two.appendChild(five);
+  two.appendChild(six);
+  five.appendChild(seven);
+  seven.appendChild(eight);
+  eight.appendChild(doubleEight);
+
+  describe('tests for functionality of append function', () => {
+
+    test('checking that append properly appends values', () => {
+      expect(seven.children).toEqual([eight]);
+    });
+
+    test('checking that invalid input to append method throws', () => {
+      expect(() => root.appendChild('bad')).toThrow();
+    });
+
+  });
+
+  describe('tests for breadthFirstFind function', () => {
+
+    test('checking that breadthFirstFind prototype finds correct node', () => {
+      expect(root.breadthFirstFind(2)).toEqual(two);
+    });
+
+    test('checking that breadthFirstFind prototype finds correct node in correct order', () => {
+      expect(root.breadthFirstFind(8)).toEqual(eight);
+    });
+  });
+
+  describe('tests for breadthFirstToString function', () => {
+
+    test('checking that breadthFirstToString prototype returns correct string', () => {
+      expect(root.breadthFirstToString()).toEqual('0\n1\n2\n3\n4\n5\n6\n7\n8\n8');
+    });
+
+    test('checking that breadthFirstToString prototype returns string of length one on root with no children', () => {
+      let empty = new NAryTree(0);
+      expect(empty.breadthFirstToString()).toEqual('0');
+    });
+  });
+});
--- lab-andrew/lib/n-ary-tree.js
+++ lab-andrew/lib/n-ary-tree.js
@@ -1,50 +1,70 @@
 'use strict';
 
-class NArySearchTree {
-  constructor(value) {
-    this.value = value;
-    this.children = [];
-  }
+const NArySearchTree = function(value) {
+  this.value = value;
+  this.children = [];
+};
 
-  appendChild(tree) {
-    if (!(tree instanceof NArySearchTree)){
-      throw new TypeError('argument must be an instance of the NArySearchTree');
+NArySearchTree.prototype.appendChild = function(tree) {
+  if (!(tree instanceof NArySearchTree)){
+    throw new TypeError('argument must be an instance of the NArySearchTree');
+  }
+  this.children.push(tree);
+};
+
+
+NArySearchTree.prototype.breadthFirstFind = function(value) {
+  let queue = [];
+  queue.push(this);
+
+  let current = null; 
+
+  while (queue.length > 0) {
+    current = queue.shift();
+    if (current.value === value){
+      return current;
     }
-    this.children.push(tree);
-  }
 
-
-  breadthFirstFind() {
-    let queue = [];
-    queue.push(this);
-
-    let current = null;
-
-    while (queue.length > 0) {
-      current = queue.shift();
-
-      console.log(`visiting ${current.value}`);
-      for (let child of current.children) {
-        queue.push(child);
-      }
-    }
-  }
-
-  depthFirstFind() {
-    let stack = [];
-    stack.push(this);
-
-    let current = null;
-
-    while (stack.length > 0) {
-      current = stack.pop();
-
-      console.log(`visiting ${current.value}`);
-      for (let child of current.children) {
-        stack.push(child);
-      }
+    for (let child of current.children) {
+      queue.push(child);
     }
   }
-}
+};
+
+NArySearchTree.prototype.breadthFirstToString = function () {
+  let queue = [];
+  queue.push(this);
+
+  let current = null;
+  let strAcc = '';
+
+  while (queue.length > 0) {
+    current = queue.shift();
+    strAcc += `\n${current.value}`;
+
+    for (let child of current.children) {
+      queue.push(child);
+    }
+  }
+  return strAcc.trim();
+};
+
+NArySearchTree.prototype.depthFirstFind = function(value) {
+  let stack = [];
+  stack.push(this);
+
+  let current = null;
+
+  while (stack.length > 0) {
+    current = stack.pop();
+    if (current.value === value){
+      return current;
+    }
+
+    for (let child of current.children) {
+      stack.push(child);
+    }
+  }
+};
 
 module.exports = NArySearchTree;
