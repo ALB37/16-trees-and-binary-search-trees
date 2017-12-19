db96c6c4331225645edd03bdadf9c22e2c504663
Andrew Bloom <blooma116@gmail.com>
2017-12-19 00:27:02 -0800

    most scenarios tested for binary search tree delete

--- lab-andrew/lib/bst.js
+++ lab-andrew/lib/bst.js
@@ -8,6 +8,9 @@ class BinarySearchTree {
   }
 
   insert(value) {
+    if (typeof value !== 'number'){
+      throw new TypeError('value must be a number');
+    }
     if (value === this.value){
       throw new Error('value already exists in BST');
     }
@@ -34,27 +37,78 @@ class BinarySearchTree {
       if (this.right) {
         return this.right.find(value);
       } else {
-        return null;
+        return -1;
       }
     }
 
     if (this.left) {
       return this.left.find(value);
     } else {
-      return null;
+      return -1;
     }
 
   }
 
-  remove() {
-    
+  _findAndRemove(value) {
+
+    if (value > this.value) {
+      if (this.right.value === value){
+        if (!this.right.left && this.right.right){
+          return this.right = this.right.right;
+        }
+        if (this.right.left && !this.right.right){
+          return this.right = this.right.left;
+        }
+        return this.right = null;
+      }
+      return this.right._findAndRemove(value);
+    }
+
+    if (this.left.value === value) {
+      if (!this.left.left && this.left.right) {
+        return this.left = this.left.right;
+      }
+      if (this.left.left && !this.left.right) {
+        return this.left = this.left.left;
+      }
+      return this.left = null;
+    }
+    return this.left._findAndRemove(value);
   }
 
-  findMinValue() {
+  _findMinValue() {
     if (this.left){
-      return this.left.findMinValue();
-    } else {
-      return this.value;
+      return this.left._findMinValue();
+    }
+    return this.value;
+  }
+
+  remove(value) {
+    let node = this.find(value);
+    if (node === -1){
+      return -1;
+    }
+    if (node.left && node.right){
+      node.value = node.right._findMinValue();
+      if (node.value === node.right.value){
+        return node.right = null;
+      }
+      return node.right._findAndRemove(node.value);
+    }
+    if ((node.left && !node.right) || (!node.left && node.right)){
+      if (node === this){
+        if (node.left){
+          return node = node.left;
+        }
+        return node = node.right;
+      }
+      return this._findAndRemove(value);
+    }
+    if (!node.left && !node.right){
+      if (node === this){
+        return null;
+      } 
+      return this._findAndRemove(value);
     }
   }
 }
