3aec3500571aaf06be13a02e6a07e3abf7433334
Andrew Bloom <blooma116@gmail.com>
2017-12-19 16:55:38 -0800

    random changes

--- lab-andrew/lib/bst.js
+++ lab-andrew/lib/bst.js
@@ -1,50 +1,50 @@
 'use strict';
 
 class BinarySearchTree {
-  constructor(value) {
+  constructor(value){
     this.value = value;
     this.left = null;
     this.right = null;
   }
 
-  insert(value) {
+  insert(value){
     if (typeof value !== 'number'){
       throw new TypeError('value must be a number');
     }
     if (value === this.value){
       throw new Error('value already exists in BST');
     }
-    if (value > this.value) {
-      if (!this.right) {
+    if (value > this.value){
+      if (!this.right){
         return this.right = new BinarySearchTree(value);
       } else {
         return this.right.insert(value);
       }
     } 
-    if (!this.left) {
+    if (!this.left){
       return this.left = new BinarySearchTree(value);
     } else {
       return this.left.insert(value);
     }
   }
 
-  find(value) {
-    if (typeof value !== 'number') {
+  find(value){
+    if (typeof value !== 'number'){
       throw new TypeError('value must be a number');
     }
-    if (value === this.value) {
+    if (value === this.value){
       return this;
     }
 
-    if (value > this.value) {
-      if (this.right) {
+    if (value > this.value){
+      if (this.right){
         return this.right.find(value);
       } else {
         return -1;
       }
     }
 
-    if (this.left) {
+    if (this.left){
       return this.left.find(value);
     } else {
       return -1;
@@ -52,9 +52,9 @@ class BinarySearchTree {
 
   }
 
-  _findAndRemove(value) {
+  _findAndRemove(value){
 
-    if (value > this.value) {
+    if (value > this.value){
       if (this.right.value === value){
         if (!this.right.left && this.right.right){
           return this.right = this.right.right;
@@ -67,11 +67,11 @@ class BinarySearchTree {
       return this.right._findAndRemove(value);
     }
 
-    if (this.left.value === value) {
-      if (!this.left.left && this.left.right) {
+    if (this.left.value === value){
+      if (!this.left.left && this.left.right){
         return this.left = this.left.right;
       }
-      if (this.left.left && !this.left.right) {
+      if (this.left.left && !this.left.right){
         return this.left = this.left.left;
       }
       return this.left = null;
@@ -79,15 +79,15 @@ class BinarySearchTree {
     return this.left._findAndRemove(value);
   }
 
-  _findMinValue() {
+  _findMinValue(){
     if (this.left){
       return this.left._findMinValue();
     }
     return this.value;
   }
 
-  remove(value) {
-    if (typeof value !== 'number') {
+  remove(value){
+    if (typeof value !== 'number'){
       throw new TypeError('value must be a number');
     }
     let node = this.find(value);
--- lab-andrew/lib/n-ary-tree.js
+++ lab-andrew/lib/n-ary-tree.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const NAryTree = function(value) {
+const NAryTree = function(value){
   this.value = value;
   this.children = [];
 };
 
-NAryTree.prototype.appendChild = function(tree) {
+NAryTree.prototype.appendChild = function(tree){
   if (!(tree instanceof NAryTree)){
     throw new TypeError('argument must be an instance of the NAryTree');
   }
@@ -13,54 +13,54 @@ NAryTree.prototype.appendChild = function(tree) {
 };
 
 
-NAryTree.prototype.breadthFirstFind = function(value) {
+NAryTree.prototype.breadthFirstFind = function(value){
   let queue = [];
   queue.push(this);
 
   let current = null; 
 
-  while (queue.length > 0) {
+  while (queue.length > 0){
     current = queue.shift();
     if (current.value === value){
       return current;
     }
 
-    for (let child of current.children) {
+    for (let child of current.children){
       queue.push(child);
     }
   }
 };
 
-NAryTree.prototype.breadthFirstToString = function () {
+NAryTree.prototype.breadthFirstToString = function (){
   let queue = [];
   queue.push(this);
 
   let current = null;
   let strAcc = '';
 
-  while (queue.length > 0) {
+  while (queue.length > 0){
     current = queue.shift();
     strAcc += `\n${current.value}`;
 
-    for (let child of current.children) {
+    for (let child of current.children){
       queue.push(child);
     }
   }
   return strAcc.trim();
 };
 
-NAryTree.prototype.depthFirstToArray = function() {
+NAryTree.prototype.depthFirstToArray = function(){
   let stack = [];
   stack.push(this);
 
   let current = null;
   let solArray = [];
 
-  while (stack.length > 0) {
+  while (stack.length > 0){
     current = stack.pop();
     solArray.push(current.value);
 
-    for (let child of current.children) {
+    for (let child of current.children){
       stack.push(child);
     }
   }
